39f5d154160c1357366b65a33b8b5b0602e33192
artin721 <artinasadzade7@gmail.com>
2024-04-30 12:01:57 +0330

    Fix 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -4,17 +4,10 @@ import { Link } from "react-router-dom";
 export default function Page404() {
   return (
     <>
-      <div className="w-full h-screen border-2 border-black rounded-sm">
+      <div className="w-full h-screen">
         <div className="w-full m-auto text-center p-8">
-          <img
-            src="../../public/Vectors/404Error2.svg"
-            alt=""
-            className="w-full  mx-auto my-10"
-          />
-          <Link
-            to={"/"}
-            className="border-black border-2 text-black rounded-lg p-3 my-10 hover:opacity-50 transition-opacity"
-          >
+          <img src="../../public/Vectors/404Error2.svg" alt="" className="w-full md:w-1/3 mx-auto" />
+          <Link to={"/"} className="border-black border-2 text-black rounded-lg p-2 hover:opacity-50 transition-opacity">
             Back To Home
             <ArrowUpRightIcon className="w-4 inline mx-1" />
           </Link>
