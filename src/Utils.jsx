f62ff0db184a470f288279a9a1bf8f3aabf444c3
artin721 <artinasadzade7@gmail.com>
2024-04-30 11:36:17 +0330

    update

--- src/Utils.jsx
+++ src/Utils.jsx
@@ -1,10 +1,7 @@
 import Users from "./data/Users";
 const userLogin = (userName, userPassword) => {
   return Users.some(
-    (user) =>
-      userPassword === user.password &&
-      user.admin &&
-      (user.gmail === userName || user.userName === userName)
+    (user) => userPassword === user.password && user.admin && (user.gmail === userName || user.userName === userName)
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { userLogin } from "./../Utils";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 export default function Login() {
-  const { userName, setUserName, userPassword, setUserPassword } =
-    useContext(UserContext);
+  const { userName, setUserName, userPassword, setUserPassword } = useContext(UserContext);
 
   let isLogin = userLogin(userName, userPassword);
 
@@ -14,35 +13,23 @@ export default function Login() {
       alert("You do not have access");
     }
   };
-
-  const changeUserNameValue = (e) => {
-    setUserName(e.target.value);
-  };
-  const changePasswordValue = (e) => {
-    setUserPassword(e.target.value);
-  };
   return (
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to admin panel
-          </h2>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to admin panel</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email Or UserName
               </label>
               <div className="mt-2">
                 <input
                   value={userName}
-                  onChange={changeUserNameValue}
+                  onChange={(e) => setUserName(e.target.value)}
                   id="email"
                   name="email"
                   type="text"
@@ -54,17 +41,14 @@ export default function Login() {
 
             <div>
               <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
               </div>
               <div className="mt-2">
                 <input
                   value={userPassword}
-                  onChange={changePasswordValue}
+                  onChange={(e) => setUserPassword(e.target.value)}
                   id="password"
                   name="password"
                   type="password"
