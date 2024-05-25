4143139baba73604ccca27efc2a6a0c5e1737a65
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-25 23:02:27 +0330

    Update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,27 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { links } from "../data/Links";
 import SideBarItems from "./SideBarItems";
+import { ShowSideBarContext } from "../context/ShowSideBarContext";
+import { useContext } from "react";
 
 export default function SideBar() {
+  const { showSideBar, setShowSideBar } = useContext(ShowSideBarContext);
+
+  const showHamburgerHandler = () => {
+    setShowSideBar((prevState) => (prevState = !prevState));
+  };
+
   return (
     <>
-      <div className="flex flex-col w-1/3 md:w-1/6">
+      <div
+        className={`${
+          showSideBar ? "block" : ""
+        }hidden fixed top-0 sm:sticky bg-white h-svh sm:flex flex-col w-1/3 md:w-1/6`}
+      >
+        <XMarkIcon
+          className="block sm:hidden w-6 my-3 mx-2"
+          onClick={showHamburgerHandler}
+        />
         <div className="sticky top-0">
           {links.map((item) => (
             <SideBarItems key={item.id} {...item} />
