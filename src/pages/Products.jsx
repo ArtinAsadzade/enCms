96596cbb8d2f62c63794de0004da8e8a84a6d58b
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-25 21:19:40 +0330

    Update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,12 +8,10 @@ export default function Products() {
 
   return (
     <div className="w-full h-svh bg-slate-200 px-5 py-10">
-      <div className="w-full flex justify-evenly bg-white m-auto p-3 rounded-lg">
+      <div className="w-full flex justify-between items-center bg-white m-auto p-3 rounded-lg">
+        <h1 className="font-bold border-b-2">Manage Products</h1>
         <button className="px-2 py-1 bg-green-600 mx-3 rounded-sm flex items-center font-bold">
           <PlusIcon className="w-5 mx-1" /> Add
-        </button>
-        <button className="px-2 py-1 bg-yellow-500 mx-3 rounded-sm flex items-center font-bold">
-          <PencilSquareIcon className="w-5 mx-1" /> Edit
         </button>
       </div>
       <div className="w-full m-auto rounded-lg my-5">
