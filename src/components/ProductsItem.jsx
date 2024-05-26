088678ab0788a1b69d497f175b5e0db1369e5f4f
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-26 16:06:19 +0330

    Update Products Item

--- src/components/ProductsItem.jsx
+++ src/components/ProductsItem.jsx
@@ -2,14 +2,21 @@ import { PencilSquareIcon, MinusIcon } from "@heroicons/react/24/outline";
 
 export default function ProductsItem(props) {
   return (
-    <div className="w-full flex items-center my-5 rounded-lg justify-between shadow-lg bg-white p-2">
-      <img src={props.img} alt="" className="w-1/7 h-20 object-cover" />
-      <p className="font-bold">{props.name}</p>
-      <div className="text-black">
-        <button className="bg-yellow-600 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
+    <div className="w-full grid grid-cols-4 items-center  my-5 rounded-lg shadow-lg bg-white p-2">
+      <img src={props.img} alt="" className="w-1/7 h-20 object-cover m-auto" />
+      <div className="text-center m-auto">
+        <p className="font-bold">{props.name}</p>
+        <p>{props.desc}</p>
+      </div>
+      <div className="text-center m-auto">
+        <p className="font-bold">{props.offerPrice}</p>
+        <p className="font-bold text-zinc-400">{props.price}</p>
+      </div>
+      <div className="text-black m-auto">
+        <button className="bg-yellow-500 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
           <PencilSquareIcon className="w-5 mx-1" />
         </button>
-        <button className="bg-red-600 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
+        <button className="bg-red-500 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
           <MinusIcon className="w-5 mx-1" />
         </button>
       </div>
