fa3c279db89cdf0a71e26194ed63010417cdd0b3
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-26 14:12:16 +0330

    Update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,6 +2,7 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 import ProductsItem from "../components/ProductsItem";
 import { productsData } from "../data/ProductsData";
 import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Products() {
   const [products] = useState(productsData);
@@ -10,15 +11,19 @@ export default function Products() {
     <div className="w-full h-svh bg-slate-200 px-5 py-10">
       <div className="w-full flex justify-between items-center bg-white m-auto p-3 rounded-lg">
         <h1 className="font-bold border-b-2">Manage Products</h1>
-        <button className="px-2 py-1 bg-green-600 mx-3 rounded-sm flex items-center font-bold">
+        <Link
+          to={"/products/newProduct"}
+          className="px-2 py-1 bg-green-600 mx-3 rounded-sm flex items-center font-bold"
+        >
           <PlusIcon className="w-5 mx-1" /> Add
-        </button>
+        </Link>
       </div>
       <div className="w-full m-auto rounded-lg my-5">
         {products.map((item) => (
           <ProductsItem key={item.id} {...item} />
         ))}
       </div>
+      <Outlet />
     </div>
   );
 }
