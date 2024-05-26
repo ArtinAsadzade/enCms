9c1e5069ed64f2c28844b91768da0296d926625a
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-26 15:12:27 +0330

    Update SideBar

--- src/components/SideBarItems.jsx
+++ src/components/SideBarItems.jsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function SideBarItems(props) {
+  let location = useLocation();
+  console.log(location);
+
   return (
     <div className="w-full flex flex-col p-2">
       <p className="text-gray-600 font-bold text-sm">{props.title}</p>
       {props.infos.map((item) => (
-        <Link
+        <NavLink
           key={item.id}
           to={item.link}
-          className={`w-full hover:bg-slate-200 p-1 rounded-md flex items-center justify-start gap-2`}
+          className={({ isActive }) =>
+            `w-full p-1 rounded-md flex items-center justify-start gap-2 ${
+              isActive ? "bg-slate-200" : "hover:bg-slate-100 transition-all"
+            }`
+          }
         >
           <span className="[&>*]:w-full [&>*]:h-full w-5 h-5">{item.icon}</span>
           <p>{item.name}</p>
-        </Link>
+        </NavLink>
       ))}
     </div>
   );
