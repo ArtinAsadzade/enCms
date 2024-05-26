18951c30605f005e380f6006c2652bae90819c98
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-26 16:06:27 +0330

    Update Side Bar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -16,10 +16,10 @@ export default function SideBar() {
       <div
         className={`${
           showSideBar ? "block" : ""
-        }hidden fixed top-0 sm:sticky bg-white h-svh sm:flex flex-col w-1/3 md:w-1/6`}
+        }hidden fixed top-0 sm:sticky bg-white h-svh sm:flex flex-col w-1/3 md:w-1/6 overflow-y-auto`}
       >
         <XMarkIcon
-          className="block sm:hidden w-6 my-3 mx-2"
+          className="block sm:hidden w-6 my-3 mx-2 cursor-pointer"
           onClick={showHamburgerHandler}
         />
         <div className="sticky top-0">
