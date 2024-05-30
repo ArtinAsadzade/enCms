b7e87511156282a92c8e8ea2b5bc03cce2469677
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-30 21:22:51 +0330

    Update Product Items

--- src/components/ProductsItem.jsx
+++ src/components/ProductsItem.jsx
@@ -6,17 +6,18 @@ export default function ProductsItem(props) {
       <img src={props.img} alt="" className="w-1/7 h-20 object-cover m-auto" />
       <div className="text-center m-auto">
         <p className="font-bold">{props.name}</p>
-        <p>{props.desc}</p>
       </div>
       <div className="text-center m-auto">
-        <p className="font-bold">{props.offerPrice}</p>
-        <p className="font-bold text-zinc-400">{props.price}</p>
+        <p className="font-bold">{props.price}</p>
+        {!!props.offerPrice && (
+          <p className="text-red-500">{props.offerPrice}</p>
+        )}
       </div>
       <div className="text-black m-auto">
-        <button className="bg-yellow-500 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
+        <button className="bg-yellow-400 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
           <PencilSquareIcon className="w-5 mx-1" />
         </button>
-        <button className="bg-red-500 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
+        <button className="bg-red-600 mx-3 my-2 rounded-sm flex items-center p-[2px] justify-center font-bold">
           <MinusIcon className="w-5 mx-1" />
         </button>
       </div>
