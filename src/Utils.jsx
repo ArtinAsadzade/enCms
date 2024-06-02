7b2ea05137f346a7449318befbddde49d08f961b
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-02 16:03:31 +0330

    Update

--- src/Utils.jsx
+++ src/Utils.jsx
@@ -1,7 +1,13 @@
-import Users from "./data/Users";
+import { useContext } from "react";
+import { UsersDataContext } from "./context/UsersDataContext";
 const userLogin = (userName, userPassword) => {
+  const { Users } = useContext(UsersDataContext);
+
   return Users.some(
-    (user) => userPassword === user.password && user.admin && (user.gmail === userName || user.userName === userName)
+    (user) =>
+      userPassword === user.password &&
+      user.admin &&
+      (user.gmail === userName || user.userName === userName)
   );
 };
 
