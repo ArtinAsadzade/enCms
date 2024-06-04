8e1b48bdf84def93e30ecee290b9ab42d5e44132
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-04 16:39:22 +0330

    Update

--- src/components/ProductsItem.jsx
+++ src/components/ProductsItem.jsx
@@ -8,29 +8,31 @@ export default function ProductsItem(props) {
     setProducts(products.filter((item) => item.id !== props.id));
   };
   return (
-    <div className="w-full grid grid-cols-12 border-b-2 bg-white px-8 py-2 my-2 rounded-md">
-      <img
-        src={props.img}
-        alt=""
-        className="w-1/7 h-20 text-left col-span-3 object-cover"
-      />
-      <div className="m-auto col-span-3 text-center w-full">
-        <p className="font-bold">{props.name}</p>
+    <>
+      <div className="w-full grid grid-cols-12 border-b-2 bg-white px-8 py-2 my-2 rounded-md">
+        <img
+          src={props.img}
+          alt=""
+          className="w-1/7 h-20 text-left col-span-3 object-cover"
+        />
+        <div className="m-auto col-span-3 text-center w-full">
+          <p className="font-bold">{props.name}</p>
+        </div>
+        <div className="w-1/4 m-auto px-2 py-1 rounded-lg col-span-3">
+          <p className="font-bold text-black">{props.price}</p>
+          {!!props.offerPrice && (
+            <p className="text-gray-400">{props.offerPrice}</p>
+          )}
+        </div>
+        <div className="text-black flex col-span-3 w-full justify-end">
+          <button className="mx-2 my-2 rounded-sm text-lg text-gray-500 flex items-center p-[2px] justify-center font-bold">
+            <PencilSquareIcon className="w-5" />
+          </button>
+          <button className="mx-2 my-2 rounded-sm text-lg text-gray-500 flex items-center p-[2px] justify-center font-bold">
+            <TrashIcon className="w-5" onClick={deleteProductHandler} />
+          </button>
+        </div>
       </div>
-      <div className="w-1/4 m-auto px-2 py-1 rounded-lg col-span-3">
-        <p className="font-bold text-black">{props.price}</p>
-        {!!props.offerPrice && (
-          <p className="text-gray-400">{props.offerPrice}</p>
-        )}
-      </div>
-      <div className="text-black flex col-span-3 w-full justify-end">
-        <button className="mx-2 my-2 rounded-sm text-lg text-gray-500 flex items-center p-[2px] justify-center font-bold">
-          <PencilSquareIcon className="w-5" />
-        </button>
-        <button className="mx-2 my-2 rounded-sm text-lg text-gray-500 flex items-center p-[2px] justify-center font-bold">
-          <TrashIcon className="w-5" onClick={deleteProductHandler} />
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
