2645e337e0573d43949f8cb99a5894788ade8bc2
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-05 14:26:36 +0330

    Update

--- src/components/ProductsItem.jsx
+++ src/components/ProductsItem.jsx
@@ -1,14 +1,31 @@
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductDataContext } from "../context/ProductDataContext";
+import YesOrNo from "./YesOrNo";
 
 export default function ProductsItem(props) {
+  const [show, setShow] = useState(false);
   const { products, setProducts } = useContext(ProductDataContext);
+
   const deleteProductHandler = () => {
     setProducts(products.filter((item) => item.id !== props.id));
+    setShow((prevState) => (prevState = !prevState));
   };
+
+  const openYesOrNoModal = () => {
+    setShow((prevState) => (prevState = !prevState));
+  };
+
   return (
     <>
+      <YesOrNo
+        title={`You Want Delete *${props.name}*?`}
+        desc={`Are you sure about deleting the desired product?`}
+        button={true}
+        func={deleteProductHandler}
+        show={show}
+        setShow={setShow}
+      />
       <div className="w-full grid grid-cols-12 border-b-2 bg-white px-8 py-2 my-2 rounded-md">
         <img
           src={props.img}
@@ -29,7 +46,7 @@ export default function ProductsItem(props) {
             <PencilSquareIcon className="w-5" />
           </button>
           <button className="mx-2 my-2 rounded-sm text-lg text-gray-500 flex items-center p-[2px] justify-center font-bold">
-            <TrashIcon className="w-5" onClick={deleteProductHandler} />
+            <TrashIcon className="w-5" onClick={openYesOrNoModal} />
           </button>
         </div>
       </div>
