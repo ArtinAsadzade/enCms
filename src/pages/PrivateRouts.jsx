3c58d65903eae670c46ec07fa8674210f02eb195
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-05 14:27:05 +0330

    Update

--- src/pages/PrivateRouts.jsx
+++ src/pages/PrivateRouts.jsx
@@ -1,23 +1,13 @@
 import { useContext } from "react";
 import { userLogin } from "../Utils";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import TopBar from "../components/TopBar";
 import SideBar from "../components/SideBar";
-import { ShowYesOrNoContext } from "../context/ShowYesOrNoContext";
-import YesOrNo from "../components/YesOrNo";
 
 export default function PrivateRouts() {
   const { userName, userPassword } = useContext(UserContext);
   const isLogin = userLogin(userName, userPassword);
-  const { setShow } = useContext(ShowYesOrNoContext);
-  const navigate = useNavigate();
-
-  const logOutHandler = () => {
-    localStorage.clear();
-    navigate("/");
-    setShow((prevState) => (prevState = !prevState));
-  };
 
   return (
     <>
@@ -26,12 +16,6 @@ export default function PrivateRouts() {
           <TopBar />{" "}
           <div className="flex mt-[2px]">
             <SideBar /> <Outlet />
-            <YesOrNo
-              title={"Log Out!"}
-              desc={`Are You Sure For Log Out?`}
-              button={true}
-              func={logOutHandler}
-            />
           </div>
         </>
       ) : (
