61c166dae3a811c5fe1b0fbef04bf9d72a6e7249
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-05 14:26:32 +0330

    Add Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blur: {
+          "0%": { filter: "blur(5px)" },
+          "100%": { filter: "blur(0px)" },
+        },
+      },
+      animation: {
+        blur: "blur 1s linear",
+      },
+    },
   },
   plugins: [],
 };
