80f4a29832f632f572cdb6cd3b9e52631330281f
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-12 21:48:28 +0330

    Update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,7 +43,7 @@ export default function Login() {
     if (JSON.parse(localStorage.getItem("user"))) {
       navigate("/home");
     }
-  }, [JSON.parse(localStorage.getItem("user"))]);
+  }, [navigate]);
 
   return (
     <>
